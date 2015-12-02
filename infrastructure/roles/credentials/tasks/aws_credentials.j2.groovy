f10dff9f449e5ce9a451b73ccfc77d46737a028a
Pedro Roque Marques <roque@juniper.net>
2015-12-01 20:55:36 -0800

    Use username/passwd rather than aws-credentials.

--- infrastructure/roles/credentials/tasks/aws_credentials.j2.groovy
+++ infrastructure/roles/credentials/tasks/aws_credentials.j2.groovy
@@ -1,15 +1,15 @@
 import jenkins.model.*
 import com.cloudbees.plugins.credentials.*
 import com.cloudbees.plugins.credentials.domains.*
-import com.cloudbees.jenkins.plugins.awscredentials.*
+import com.cloudbees.plugins.credentials.impl.*
 
 domain = Domain.global()
 provider = Jenkins.instance.getExtensionList("com.cloudbees.plugins.credentials.SystemCredentialsProvider")[0]
 store = provider.getStore()
 
-// list = store.getCredentials(domain)
-// exists = list.any{ it instanceof AWSCredentialsImpl && ((AWSCredentialsImpl) it).getId() == "k8s-provisioner" }
-
-credentials = new AWSCredentialsImpl(CredentialsScope.GLOBAL, "k8s-provisioner", "{{ aws_access_key_id }}", "{{ aws_secret_access_key }}", "k8s provisioner")
+credentials = new UsernamePasswordCredentialsImpl(
+    CredentialsScope.GLOBAL, "k8s-provisioner", "k8s provisioner",
+    "{{ aws_access_key_id }}",
+    "{{ aws_secret_access_key }}")
 store.addCredentials(domain, credentials)
 provider.save()
